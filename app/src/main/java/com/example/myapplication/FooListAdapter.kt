17a8783cba7a19d4ed422c95a58d0eac4b02714c
G-moog <kingmumuop@gmail.com>
2023-11-01 17:15:10 +0900

    사진 나오긴 한다. 근데 폴더 분류 엉망임

--- app/src/main/java/com/example/myapplication/FooListAdapter.kt
+++ app/src/main/java/com/example/myapplication/FooListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.sample.databinding.ItemFooBinding
 
 
@@ -37,6 +38,9 @@ class FooListAdapter(
             binding.apply {
                 tvName.text = item.name
                 tvPhone.text = item.phone
+                Glide.with(itemView)
+                    .load(item.imgUri)
+                    .into(ivPicture)
 
                 itemView.setOnClickListener {
                     onItemClick(item)
