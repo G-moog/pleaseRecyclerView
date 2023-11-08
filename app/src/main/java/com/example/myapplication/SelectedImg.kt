2bc875966acad21312882a4c164591c0772d63a7
G-moog <kingmumuop@gmail.com>
2023-11-08 17:35:02 +0900

    아몰라

--- app/src/main/java/com/example/myapplication/SelectedImg.kt
+++ app/src/main/java/com/example/myapplication/SelectedImg.kt
@@ -6,20 +6,14 @@ import android.util.Log
 
 data class SelectedImg(
     val id: Int,
-    val imgUriName: String,
-    val imgFolderName: String,
+
     val imgUri : Uri,
-    var isChecked : Boolean = false
+
 ) {
     // 더미
     companion object {
-        fun createSamples(page : Int, imgUriList : MutableList<Uri>,  folderNameList : MutableList<String>) = mutableListOf<SelectedImg>().apply {
-            for (i in 0 until 9) {
-                val number = page * 10
-                if(page+i < 0){
-                    Log.d(TAG, "인덱스가 -1일 때 page 값 : " + page)
-                    Log.d(TAG, "인덱스가 -1일 때 i 값 : " + i)
-                }
+        fun createSamples(imgUriList : MutableList<Uri>) = mutableListOf<SelectedImg>().apply {
+            for (i in 0 until imgUriList.size) {
                 add(SelectedImg(number +i, "Uri: ${imgUriList.get(page+i)}", "폴더: ${folderNameList.get(page+i)}", imgUriList.get(page + i)))
                 Log.d(TAG, "createSamples 할 때 뷰홀더에 찍은 이미지 Uri: " + imgUriList.get(page + i))
                 Log.d(TAG, "createSamples 할 때 뷰홀더에 찍은 이미지 폴더명: " + folderNameList.get(page + i))
